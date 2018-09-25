cff3553e3801c65daac8bc62396274b3bd0bc2e3
Inccubus <34146602+blackshooter987@users.noreply.github.com>
2018-09-24 22:39:48 -0300

    Att

--- nossos-scripts/tsp.c
+++ nossos-scripts/tsp.c
@@ -1,6 +1,5 @@
 #include "tsp.h"
 
-#define distance(v1,v2) round(sqrt(pow((v2->x - v1->x),2) + pow((v2->y - v1->y),2)))
 #define number_of_edges(n) ((n/2)*(n-1))+(((n-1)/2)*((n%2) != 0))
 #define Default 30
 
@@ -22,11 +21,19 @@ struct edge{
     int dist;
 };
 
+//Estrutura de adjacencias de cada cidade
 typedef struct{
     int id,color;
     linked_list* adjacencies;
 }Adj;
+//Calculo das distancias entre cidades
+static int distance(City* v1, City* v2){
+    int x = pow((v2->x - v1->x),2);
+    int y = pow((v2->y - v1->y),2);
 
+    return round(sqrt(x+y));
+}
+//Criação do vetor de adjacencias
 Adj* make_Adj(int n){
     Adj* x = malloc(sizeof(Adj)*n);
     for(int i = 0; i < n; i++){
@@ -36,19 +43,17 @@ Adj* make_Adj(int n){
     }
     return x;
 }
-
+//Liberação do vetor de adjacencias
 void free_Adj(Adj* x, int n){
     for(int i = 0; i < n;i++){
         free_ll(x[i].adjacencies);
     }
     free(x);
 }
-
 //Função de comparação para o quicksort
 static int compar(const void* x1, const void* x2){
     return ((Edge*)x1)->dist > ((Edge*)x2)->dist? 1:0;
 }
-
 //Gera o vetor com todas as aresta possíveis de um problema
 Edge* make_edges(Data* x){
     int index = 0, h = number_of_edges(x->dimension);
@@ -146,7 +151,7 @@ void print_mst(Data* data, Edge* nec) {
 
     fclose(arq);
 }
-
+//Cria um arquivo de saída com o Tour
 void print_tour(Data* data, int* tr) {
     char name[Default];
     strcpy(name, "tour/");
@@ -164,14 +169,14 @@ void print_tour(Data* data, int* tr) {
 
     fclose(arq);
 }
-
+//Função usada de callback para empilhar as cidades adjacentes de uma determinada cidade
 void* visit(void* data,void* stack){
     if(((Adj*)data)->color == 0){
         push(data,(Stack*)stack);
     }
     return NULL;
 }
-
+//Tour baseado em busca em profundidade em grafos
 int* tour(Adj* x, int n){
     int* tr = malloc(sizeof(int)*n);
     Stack* st = create_Empty_Stack();
@@ -190,7 +195,6 @@ int* tour(Adj* x, int n){
     return tr;
 }
 
-
 int main(int argc, char** argv){
     //Abre o arquivo e cria as estruturas necessárias inicialmente
     FILE* x = fopen(argv[1],"r");
@@ -204,18 +208,24 @@ int main(int argc, char** argv){
     //Ordena o vetor de aresta 'b' crescentemente
     qsort(b,number_of_edges(y->dimension),sizeof(Edge),compar);
 
+    //Estrutura do Union-Find
     UF* d = UF_init(y->dimension);
+    //Vetor de Estrutura de adjacencias
     Adj* m = make_Adj(y->dimension);
-
+    //Indice do vetor de arcos que geram a mst
     int index = 0;
-    for(int i = 0; i < number_of_edges(y->dimension); i++){
+
+    //Abaixo a MST é formada
+    //O if é satisfeito quando as duas cidades daquele arco foram conectadas
+    //Para cada vez que o if é satisfeito o arco é adicionado no vetor da mst e cada cidade tem sua adjacente acrescentada a lista
+    for(int i = 0; i < number_of_edges(y->dimension) && index < (y->dimension-1); i++){
         if(UF_union(b[i].x1, b[i].x2,d)){
             add_data(m[b[i].x1 - 1].adjacencies, &m[b[i].x2 - 1]);
             add_data(m[b[i].x2 - 1].adjacencies, &m[b[i].x1 - 1]);
             mst[index++] = b[i];
-            if(index == (y->dimension-1)) break;
         }
     }
+
     //Libera (a maior) parte da memória alocada no programa, que não é mais necessária
     free(b);
     free_UF(d);
@@ -224,7 +234,6 @@ int main(int argc, char** argv){
     //Gera o vetor do tour e gera o arquivo de saida do tour
     int *tr = tour(m,y->dimension);
     print_tour(y,tr);
-
 
     //Libera todo o resto da memória alocada no programa
     free_Adj(m,y->dimension);
